641553ab4857636a28b24337fee67ed7bda35df3
Jakub Wąsowski <jakub@nerdz.pl>
2024-10-24 19:09:21 +0200

    feat: implement groq

--- src/index.js
+++ src/index.js
@@ -1,20 +1,32 @@
-const { Telegraf } = require('telegraf');
-require('dotenv').config();
+const { Telegraf } = require("telegraf");
+const { Groq } = require("groq-sdk");
+require("dotenv").config();
 
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.start((ctx) => ctx.reply('Welcome to your AI Assistant!'));
+bot.start((ctx) => ctx.reply("Welcome to your AI Assistant!"));
 
-bot.on('message', (ctx) => {
+bot.on("message", async (ctx) => {
+  const aiResponse = await groq.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: ctx.message.text,
+      },
+    ],
+    model: "llama3-8b-8192",
+  });
 
+  const aiResponseText =
+    aiResponse.choices[0]?.message?.content || "No response";
 
-    ctx.reply('Napisałeś mi: ' + ctx.message.text);
-
-})
+  ctx.reply(aiResponseText);
+});
 
 bot.launch();
 
-console.log('Bot started');
+console.log("Bot started");
 
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
