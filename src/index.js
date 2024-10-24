da2728e66dd00a3b2f8abbf6dc08bd8b697b03af
Jakub Wąsowski <jakub@nerdz.pl>
2024-10-24 18:53:33 +0200

    feat: telegram setup

--- src/index.js
+++ src/index.js
@@ -5,7 +5,16 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start((ctx) => ctx.reply('Welcome to your AI Assistant!'));
 
+bot.on('message', (ctx) => {
+
+
+    ctx.reply('Napisałeś mi: ' + ctx.message.text);
+
+})
+
 bot.launch();
+
+console.log('Bot started');
 
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
